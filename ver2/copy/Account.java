b8d40be8e369c69f2fa8dd2b0175e500d25d632c
kahuynh05 <kahuynh@valdosta.edu>
2024-09-01 22:09:26 -0400

    Update Account.java

--- ver2/copy/Account.java
+++ ver2/copy/Account.java
@@ -3,7 +3,7 @@ package ver2.copy;
 public class Account {
 	// Instance variable, only available inside this class.
 		private double balance;
-
+// A Comment.
 		private String name;
 		
 		public String getName() {
